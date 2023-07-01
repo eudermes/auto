6ec12e8941761a013a28adf3fb31be1dbd662fb2
eudermes <eudermes11@gmail.com>
2023-07-01 09:34:52 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,19 @@ import React, { useState, useEffect } from "react";
 export default function Home() {
 
 
-  const [anoCarroNovo, setAnoCarroNovo] = useState("");
+  const [anoCarroNovo, setAnoCarroNovo] = useState(2023);
   const [valorCarroNovo, setValorCarroNovo] = useState("");
   const [valorCarroVelho, setValorCarroVelho] = useState("");
   const [valorEntrada, setValorEntrada] = useState("");
   const [agil, setAgil] = useState("");
   const [quotaMax, setQuotaMax] = useState<number>(0);
-  
-  const idadeCarro = 2023 - parseInt(anoCarroNovo);
-  
+  const [prazoMax, setPrazoMax] = useState<number>(0);
+  const [txJuros, setTxJuros] = useState<number>(0);
+  const [produto, setProduto] = useState<number>(0);
 
+
+  const idadeCarro = (2023 - parseInt(anoCarroNovo));
+  
   const handleanoCarroNovoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setAnoCarroNovo(event.target.value);
   }
@@ -36,6 +39,29 @@ const handlevalorEntradaChange = (event: React.ChangeEvent<HTMLInputElement>) =>
     setAgil(event.target.value);
   }
 
+  useEffect(() => {
+    if(idadeCarro <=2){setQuotaMax(0.8), setPrazoMax(60), setTxJuros(2.92)}
+  
+    if(idadeCarro >=3 && idadeCarro <=5){setQuotaMax(0.6), setPrazoMax(60), setTxJuros(4.26)}
+
+    if(idadeCarro >=6 && idadeCarro <=8){setQuotaMax(0.5),setPrazoMax(48), setTxJuros(2.34)}
+
+    if(idadeCarro ==9){setQuotaMax(0.5),setPrazoMax(36), setTxJuros(2.34)}
+
+    if(idadeCarro ==10){setQuotaMax(0.5),setPrazoMax(24), setTxJuros(2.34)}
+
+    if(idadeCarro > 10){setQuotaMax(0), setPrazoMax(0), setTxJuros(0) }
+  
+  }, [idadeCarro]);
+
+
+  const handleCalcularProduto = () => {
+     const valorCarroNovoFloat = parseFloat(valorCarroNovo);
+     const produtoCalculado = valorCarroNovoFloat * quotaMax;
+    setProduto(produtoCalculado);
+      };
+    
+      
   return (
 
     <div className=' bg-blue-400'>
@@ -117,9 +143,25 @@ const handlevalorEntradaChange = (event: React.ChangeEvent<HTMLInputElement>) =>
       </div>
 
       <div>
-      {idadeCarro > 0 && <p className='p-1 pt-1 mt-0 m-3 text-white'>{`idade do carro: ${idadeCarro} anos`}</p>}
+      {idadeCarro >= 0 && <p className='p-1 pt-1 mt-0 m-3 text-white'>{`idade do carro: ${idadeCarro} anos`}</p>}
       
-  
+
+      {quotaMax >= 0 && <p className='p-1 pt-1 mt-0 m-3 text-white'>{`a cota máxima de financiamento é ${quotaMax}`}</p>}
+      
+      {prazoMax >= 0 && <p className='p-1 pt-1 mt-0 m-3 text-white'>{`o prazo máximo de financiamento é ${prazoMax} meses`}</p>}
+      
+      {txJuros >= 0 && <p className='p-1 pt-1 mt-0 m-3 text-white'>{`a taxa de juros mensal é ${txJuros}%`}</p>}
+      
+      {idadeCarro > 10 && <p className='p-1 pt-1 mt-0 m-3 text-white'>Lamentamos Muito, mas infelizmente não financiamos veículos com mais de 10 anos. Gostaríamos de lhe oferecer nossas opções de consorcio: <a href='http://www.caixaconsorcio.com.br/consorcio'className='text-blue-900'>caixaconsorcio</a></p>}
+      
+
+
+      {idadeCarro > 10 && <p className='p-1 pt-1 mt-0 m-3 text-red-600'>Aqui deveria aparecer o produto das variáveis valorCarroNovo * quotaMax, mas não estou conseguindo</p>}
+      
+      <button onClick={handleCalcularProduto} className='text-white'>Calcular</button>
+
+      <p>{produto}</p>
+    
       </div>
 
 
