8bb398cdd1e490620ad3a0917ed420b302f97948
eudermes <eudermes11@gmail.com>
2023-07-03 17:20:49 -0300

    com fotos dos veículos de forma manual

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,49 @@
 
 "use client";
-import Image from 'next/image';
+
 import React, { useState, useEffect } from "react";
+
 
 export default function Home() {
 
+
+const AUDI80="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5dvfW78JxXCzsx7ynA2rdy350qivt2eKZVA4t7mmz&s";
+const AUDI100="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA5rDkpdAtx66Yet5zzgVlx9t4U_Iwh2fTfOdNKaSt-Q&s";
+const AUDIA1="https://www.webmotors.com.br/carros/estoque/audi/a1";
+const AUDIA1QU="https://image.webmotors.com.br/_fotos/AnuncioUsados/gigante/2023/202304/20230420/audi-a1-1.4-tfsi-sport-16v-185cv-gasolina-2p-automatico-wmimagem17102309680.jpg?s=fill&w=249&h=186&q=70";
+const AUDIA3="https://audimediacenter-a.akamaihd.net/system/production/media/90567/images/72391bd2d21a80a761f0df1bd5bff197d5804daa/A201895_blog.jpg?1583155437";
+const AUDIA3SP="https://fotos.jornaldocarro.estadao.com.br/wp-content/uploads/2021/11/23115438/Audi-A3-Sportback-S-Line-13-1160x653.jpg";
+const AUDIA4SE="https://3.bp.blogspot.com/-l5fyrzofywY/XNtf7MumLpI/AAAAAAAAUgU/ojb-WB3NpVwokb890FVbMXVSFRCSSENMACLcBGAs/s1600/Novo-Audi-A4-2020%2B%25284%2529.jpg";
+const AUDIA7="https://image.webmotors.com.br/_fotos/AnuncioUsados/gigante/2023/202306/20230616/AUDI-A7-3.0-55-TFSI-GASOLINA-PERFORMANCE-QUATTRO-4P-STRONIC-wmimagem09022201735.jpg?s=fill&w=249&h=186&q=70";
+const AUDIA8="https://cdn.motor1.com/images/mgl/W89egj/367:0:2188:1642/2023-audi-r8-v10-gt-rwd-exterior.webp";
+const AUDIA8LW="https://motorshow.com.br/wp-content/uploads/sites/2/2015/08/img-365237-audi-a8-l-w12.jpg";
+const AUDIQ3="https://quatrorodas.abril.com.br/wp-content/uploads/2022/01/02__DSC5619-Bearbeitet-e1643936180573.jpg?quality=70&strip=info&w=1280&h=720&crop=1";
+const AUDIQ5="https://cdn.motor1.com/images/mgl/0eJWgm/s3/audi-q5-sportback-tfsie-2023.jpg";
+const AUDIQ7="https://cdn.motor1.com/images/mgl/wl2rNQ/s1/2024-audi-q7-facelift-rendering.jpg";
+const AUDIR8="https://cdn.motor1.com/images/mgl/xqZoAk/s3/2023-audi-r8-v10-gt-rwd.jpg";
+const AUDIR8GT="https://s.glbimg.com/jo/g1/f/original/2011/06/03/audi-2-620.jpg";
+const AUDIRS3="https://www.revistafullpower.com.br/wp-content/uploads/2021/07/rs01.jpg";
+const AUDIRS4A="https://image1.mobiauto.com.br/images/api/images/v1.0/62686222/transform/fl_progressive,f_webp,q_70";
+const AUDIRS5="https://rodasdeligaleve.com.br/media/mageplaza/blog/post/r/o/rodas-raw-a23-modelo-audi-rs5.jpg";
+const AUDIRS6="https://motorshow.com.br/wp-content/uploads/sites/2/2021/07/audi-rs-6-avant-2021-1.jpg";
+const AUDIS3="https://www.automaistv.com.br/wp-content/uploads/2022/07/Audi-S3-1_edited-990x594.jpg";
+const AUDIS4SE="https://1.bp.blogspot.com/-8SLT0tfBJ7s/XQz1ADy7gFI/AAAAAAACwBc/Yfgz7VM6a_8iv_6KLz2Y_L7Or8v9XUy8wCLcBGAs/s1600/novo-audi-s4-2020%2B%25284%2529.jpg";
+const AUDIS6AV="https://www.automaistv.com.br/wp-content/uploads/2022/03/audi_a6_avant_e-tron_concept_89_edited-990x593.jpg";
+const AUDIS7="https://www.motortrend.com/uploads/2022/07/2023-Audi-S7-02-front-three-quarter-view.jpg";
+const AUDIS8="https://cdn.motor1.com/images/mgl/8QMwo/s3/2022-audi-a8-l.jpg";
+const AUDITT="https://revistacarro.com.br/wp-content/uploads/2020/12/Audi-TT-RS_4.jpg";
+const AUDITTRO="https://quatrorodas.abril.com.br/wp-content/uploads/2016/11/5658c582de40d64c2037ecccaudi-tts-roadster-2015.jpeg?quality=70&strip=all";
+const AUDITTS="https://quatrorodas.abril.com.br/wp-content/uploads/2023/02/1109060-e1676667092474.jpg?quality=70&strip=info&w=1280&h=720&crop=1";
+const BMWRX="https://quatrorodas.abril.com.br/wp-content/uploads/2023/02/1109060-e1676667092474.jpg?quality=70&strip=info&w=1280&h=720&crop=1";
+const BMW118="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/2018_BMW_118i_SE_Automatic_1.5.jpg/1024px-2018_BMW_118i_SE_Automatic_1.5.jpg";
+const BMW130="https://www.mazettoseguros.com.br/blog/wp-content/uploads/2018/04/seguro-bmw-130i.jpg";
+const BMW318="https://image.webmotors.com.br/_fotos/AnuncioUsados/gigante/2022/202208/20220808/bmw-318i-2.0-sport-16v-gasolina-4p-automatico-WMIMAGEM12581722871.jpg?s=fill&w=249&h=186&q=70";
+const BMW520="https://www.razaoautomovel.com/wp-content/uploads/2019/09/BMW-520d-1_925x520_acf_cropped.jpeg";
+const BMW530="https://bocamafrapremium.com.br/wp-content/uploads/2022/12/e1085437ea654060ae7b9e6a7fd9ce3c_1657630837337.jpg";
+
+const arrayDeVariaveis = [AUDI80, AUDI100, AUDIA1, AUDIA1QU, AUDIA3, AUDIA3SP, AUDIA4SE, AUDIA7, AUDIA8, AUDIA8LW, AUDIQ3, AUDIQ5, AUDIQ7, AUDIR8, AUDIR8GT, AUDIRS3, AUDIRS4A, AUDIRS5, AUDIRS6, AUDIS3, AUDIS4SE, AUDIS6AV, AUDIS7, AUDIS8, AUDITT, AUDITTRO, AUDITTS, BMWRX, BMW118, BMW130, BMW318, BMW520, BMW530
+
+]
 
   const [anoCarroNovo, setAnoCarroNovo] = useState<number>(2023);
   const [valorCarroNovo, setValorCarroNovo] = useState<number>(0);
@@ -24,7 +63,15 @@ export default function Home() {
   const [valorDisponivelParcela, setValorDisponivelParcela] = useState<number>(0);
   const [percentualComprometimento, setPercentualComprometimento] = useState<number>(0);
   const [percentualComprometimentoX100, setPercentualComprometimentoX100] = useState<number>(0);
+  
+ 
+  
 
+  const marcaModeloCarro = BMW130;
+
+  {/*const fotoCarroFiltrada = arrayDeVariaveis.filter(variavel => variavel === marcaModeloCarro);*/}
+
+  
   const idadeCarro = (2023 - parseInt(anoCarroNovo));
 
   const handleComprometimentoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -90,9 +137,6 @@ const handlevalorEntradaChange = (event: React.ChangeEvent<HTMLInputElement>) =>
           const valorMaxFloat = parseFloat(valorMax);
           const subAcressimoEntrada = valorSolicitadoFloat - valorMaxFloat
 
-        
-
-
          setValorMax(produtoCalculado), setValorCarroVelhoAgil(produtoValorCarroVelhoAgil), setValorSolicitado(somaValorSolicitado), setAcressimoEntradda(subAcressimoEntrada);
            };
   
@@ -112,11 +156,12 @@ const handlevalorEntradaChange = (event: React.ChangeEvent<HTMLInputElement>) =>
            setValorDisponivelParcela(opvalorDisponivelParcela), setPercentualComprometimento(opPercentualComprometimento),setPercentualComprometimentoX100(opPercentualComprometimentoX100);
              };
 
-  
-  
-         return (
+             return (
+                
+    <div className=' bg-blue-400'> 
 
-    <div className=' bg-blue-400'>
+{/*--------------------------------------------------------------------------------------------------*/}
+
     <h1 className='text-center text-white text-3xl font-bold p-5'>PARÂMETROS FINANCIAMENTO:</h1>
     
     <div className='w-full flex justify-center space-x-40'>
@@ -286,8 +331,13 @@ const handlevalorEntradaChange = (event: React.ChangeEvent<HTMLInputElement>) =>
 
       {percentualComprometimento > 0.3 && <p>O seu percentual de Comprometimento com pagamento de financiamentos e empréstimo já encontra se acima dos 30%, ele está hoje com {percentualComprometimento}</p>}  
 
-         
+      
+      
+      
+      {/*<p>{fotoCarroFiltrada}</p>*/}     
+     
+      <img src={marcaModeloCarro} alt="Imagem" className="w-36 h-auto" />
 
     </div>
-  );
-}
+  
+)}
